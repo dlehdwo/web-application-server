db0fe3bdcbe5b8609fdb7f4e8df0fbdcc42c28d9
LeeDongjae <leedongjae625@gmail.com>
2025-07-15 19:15:32 +0900

    요구사항3 테스트코드 작성

--- src/test/java/webserver/RequestHandlerTest.java
+++ src/test/java/webserver/RequestHandlerTest.java
@@ -24,4 +24,12 @@ class RequestHandlerTest {
         User expected = new User("aaa", "bbb", "dongjae", "lee%40gmail");
         assertThat(answer).usingRecursiveComparison().isEqualTo(expected);
     }
+
+    @Test
+    @DisplayName("요구사항2 POST으로 회원가입하기")
+    void POST으로회원가입하기() {
+        User answer = requestHandler.signUpPost("userId=aaa&password=bbb&name=dongjae&email=lee%40gmail");
+        User expected = new User("aaa", "bbb", "dongjae", "lee%40gmail");
+        assertThat(answer).usingRecursiveComparison().isEqualTo(expected);
+    }
 }
